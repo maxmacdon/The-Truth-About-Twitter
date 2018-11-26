3d4d7f1b64e6d30ad8ea71a579a9fbacc13b3136
Max Macdonald <C15740661@mydit.ie>
2018-11-26 15:14:16 +0000

    Hitting Twitter API and returning specified users last 20 tweets

--- twitterstruth/views.py
+++ twitterstruth/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-
+from django.conf import settings
 from .forms import HandleForm
 import json
+import tweepy
+import os
 
 
 def index(request):
-    username = ''
+    tweets = None
     if request.method == 'POST':
         form = HandleForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
-            with open("twitter_credentials.json", "r") as file:
+
+            file_name = os.path.join(settings.BASE_DIR, 'twitterstruth\\credentials\\twitter_credentials.json')
+            with open(file_name, "r") as file:
                 creds = json.load(file)
+
+            auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+            auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+
+            api = tweepy.API(auth)
+            tweets = api.user_timeline(username)
     else:
         form = HandleForm()
 
-    return render(request, 'index.html', {'form': form, 'username': username})
+    return render(request, 'index.html', {'form': form, 'tweets': tweets})
 
 
