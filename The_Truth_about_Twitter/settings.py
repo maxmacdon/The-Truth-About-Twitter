e5243d74523d5b7b4d8c2f0cc2f7e15143489d80
Max Macdonald <C15740661@mydit.ie>
2018-11-28 14:30:03 +0000

    Set up PostgreSQL database and connected this app to it

--- The_Truth_about_Twitter/settings.py
+++ The_Truth_about_Twitter/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'The_Truth_about_Twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangofyp',
+        'USER': 'fyp',
+        'PASSWORD': 'C15740661',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
